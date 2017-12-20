f9c6ba532461c62a4abc3704b0b6f5d1a4c56719
Your Name <you@example.com>
2017-12-20 12:36:18 +0100

    Deleted pull request changes in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.conf.urls import url
 from . import views
 
@@ -9,16 +8,3 @@ urlpatterns = [
     #url(r'^post/new-review/$', views.post_new, name='post_new_review'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
-=======
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    #url(r'^post/new-review/$', views.post_new, name='post_new_review'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-]
->>>>>>> 6a2a09b7ee2b72af64ad30169749142c45881ae9
->>>>>>> 50892ee861f7e2a249e1f28027a9d4343d20b2f2
